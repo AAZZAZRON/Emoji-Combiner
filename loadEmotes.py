e6658b0a4cb8a2e9bf83ad975d20bc96fa8abba4
Aaron Zhu <72463938+AAZZAZRON@users.noreply.github.com>
2022-02-01 16:58:41 +0000

    framework

--- loadEmotes.py
+++ loadEmotes.py
@@ -1,5 +1,26 @@
+import json
+from emoteVars import dates, emojis
+
+
 client = None
+rootUrl = "https://www.gstatic.com/android/keyboard/emojikitchen"
+
 
 def start(c):
   client = c
-    
+  file = open("loaded.json")
+  outputData = json.load(file)
+
+
+  file.close()
+  print("Emotes Loaded")
+
+
+
+def reformat(emoji):
+  return "-".join([x.lower() for x in emoji.split("-")])
+
+
+
+
+  
